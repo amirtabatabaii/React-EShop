a9eb0c3717f960511e61809f50842a00af52c9b1
AmirAhmad Tabatabaei <57757615+amirtabatabaii@users.noreply.github.com>
2021-10-01 07:23:27 +0300

    API List

---
+++ src/utility/apiUrl.js
@@ -0,0 +1,19 @@
+// API List
+
+// Get Cat List
+export const GetCatListApi = `https://challenge.fnaghshin.com/api/cat`;
+
+// Get Cat List With Posts
+export const GetCatPostsListApi = `https://challenge.fnaghshin.com/api/catpost`;
+
+// Get Random Products
+export const GetRandomProductsApi = `https://challenge.fnaghshin.com/api/randompro/${total}`;
+// Sample : https://challenge.fnaghshin.com/api/randompro/20 return 20 product
+
+// Get One Category Product List
+export const GetOneCategoryProductApi = `https://challenge.fnaghshin.com/api/catpro/${catid}`;
+// Sample : https://challenge.fnaghshin.com/api/catpro/10 Return All category 10 products
+
+// Get Category Info With Products
+export const GetCatInfoProductsApi = `https://challenge.fnaghshin.com/api/cat/${catid}`;
+// Sample : https://challenge.fnaghshin.com/api/cat/10 Return Category Info With Post List
